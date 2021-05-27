f5ca9c6166eaa28e04063f1530b3b00dbf5dce85
Eduard Lupacescu <lupacescueduard@gmail.com>
2021-05-27 12:36:29 +0300

    wip

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,19 +3,38 @@
 $finder = Symfony\Component\Finder\Finder::create()
     ->notPath('bootstrap/*')
     ->notPath('storage/*')
-    ->notPath('/vendor')
+    ->notPath('vendor')
     ->in([
-        __DIR__.'/app',
-        __DIR__.'/config',
-        __DIR__.'/routes',
-        __DIR__.'/tests',
-        __DIR__.'/database',
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+        __DIR__ . '/database',
     ])
     ->name('*.php')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return (new PhpCsFixer\Config)->setRules(
-    config('fixer.rules')
-)->setFinder($finder);
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR2' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'not_operator_with_successor_space' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays', 'arguments', 'parameters']],
+        'phpdoc_scalar' => true,
+        'unary_operator_spaces' => true,
+        'binary_operator_spaces' => true,
+        'blank_line_before_statement' => [
+            'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
+        ],
+        'phpdoc_single_line_var_spacing' => true,
+        'phpdoc_var_without_name' => true,
+        'method_argument_space' => [
+            'on_multiline' => 'ensure_fully_multiline',
+            'keep_multiple_spaces_after_comma' => true,
+        ],
+    ])
+    ->setFinder($finder);
