dd3b5dd576caa96233e7fdcf288612c17b933d94
Eduard Lupacescu <eduard.lupacescu@binarcode.com>
2023-08-02 17:46:07 +0300

    fix: wip

---
+++ app/helpers.php
@@ -0,0 +1,49 @@
+<?php
+
+
+/**
+ * @return Str
+ */
+
+use Illuminate\Contracts\Auth\Authenticatable;
+
+if (! function_exists('str')) {
+    /**
+     * @return Str
+     */
+    function str()
+    {
+        return resolve(Str::class);
+    }
+}
+
+if (! function_exists('user')) {
+    function user(): Authenticatable
+    {
+        return Auth::user();
+    }
+}
+
+if (! function_exists('tests_path')) {
+    /**
+     * Get the path to the tests folder.
+     *
+     * @return string
+     */
+    function tests_path(string $path = ''): string
+    {
+        return app('path.base')
+            .DIRECTORY_SEPARATOR.'tests'
+            .($path !== '' && $path !== '0' ? DIRECTORY_SEPARATOR.$path : $path);
+    }
+}
+
+if (! function_exists('withoutProtocol')) {
+    /**
+     * Get the url without protocol
+     */
+    function withoutProtocol(string $fqdn = ''): string
+    {
+        return str_replace(['https://', 'http://'], ['https://' => '', 'http://' => ''], $fqdn);
+    }
+}
